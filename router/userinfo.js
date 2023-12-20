055d9fe6512224dd40a61124ec47d0a4df077654
袁澧林 <kobe980918@gmail.com>
2023-12-20 17:28:49 +0800

    更新 userinfo.js

--- router/userinfo.js
+++ router/userinfo.js
@@ -12,9 +12,33 @@ const expressJoi = require("@escook/express-joi");
 // 导入userInfo用户信息模块
 const userInfoHandler = require("../router_handle/userinfo");
 
+// 导入验证规则
+const {
+	name_limit,
+	email_limit,
+} = require('../limit/user.js')
+
+
 // 上传头像
 router.post("/uploadAvatar", userInfoHandler.uploadAvatar);
 
+// 绑定账号
+router.post("/bindAccount", userInfoHandler.bindAccount);
+
+// 获取用户信息
+router.post("/getUserInfo", userInfoHandler.getUserInfo);
+
+// 修改姓名
+router.post("/changeName", expressJoi(name_limit), userInfoHandler.changeName)
+
+// 修改性别
+router.post("/changeSex", userInfoHandler.changeSex)
+
+// 修改用户信息
+router.post("/updateUserInfo", userInfoHandler.updateUserInfo)
+
+// 修改邮箱
+router.post("/changeEmail", expressJoi(email_limit), userInfoHandler.changeEmail)
+
 // 向外暴露路由
 module.exports = router
-
